142fb58ecb90aef615aa0395bb22d98232be897c
kienht <kienht.dev@gmail.com>
2024-04-29 14:58:20 +0700

    replace

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,10 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   video: false,
   e2e: {
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require("./cypress/plugins/index.js")(on, config);
     },
   },
-})
+});
